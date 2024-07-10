dea2e905a65955d72e676a6d14c76eb25155dca2
Md Ariful Islam Babu <83685793+Ariful5469@users.noreply.github.com>
2024-07-10 12:10:24 +0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RockPaperScissorsGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length < 3)
+            {
+                Console.WriteLine("Usage: dotnet run <move1> <move2> ... <moveN>");
+                Console.WriteLine("Provide an odd number (>=3) of non-repeating moves.");
+            }
+            else
+            {
+                try
+                {
+                    List<string> moves = new List<string>(args);
+                    RockPaperScissorsGame game = new RockPaperScissorsGame(moves);
+                    game.Play();
+                }
+                catch (ArgumentException e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+        }
+    }
+}
